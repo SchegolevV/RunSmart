8e38f2ca0380a8716fd3dad4573feaa00bc40ae5
Valentin <maracas20011@gmail.com>
2022-11-30 23:37:10 +0300

    first try in js

---
+++ src/js/script.js
@@ -0,0 +1,71 @@
+// var name = "valentine";
+// let number = 7;
+// const pi = 3.14;
+
+
+
+// number = 4;
+
+// let leftBorderWidth;     -undefined
+
+//number числа
+//string "строки, всё что можно описать словами"
+//true/false
+//null 
+// undefined
+// let obj = {
+//     name: 'apple',
+//     color: 'green',
+//     weight: 200
+// }
+// Symbol
+
+// alert(1234);
+// console.log(number);
+// let answer = confirm("Вам есть 18?");
+// console.log(answer);
+
+// let answer = prompt("Вам есть 18?","");
+// console.log(answer);
+
+// console.log(4 + 'gff');
+
+// let isChecked = false,
+//     isClosed = false;
+
+// console.log(isChecked && isClosed);
+
+// console.log(isChecked || isClosed);
+
+
+
+// let answer = confirm("Вам есть 18?");
+// console.log(answer);
+
+// if (answer) {
+//     console.log('проходите')
+// } else {
+//     console.log('уходи')
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log ('неправильно')
+// } else if (num > 100) {
+//     console.log('много')
+// } else {
+//     console.log('верно')
+// }
+
+// for(let i = 1; i < 8; i++) {
+//     console.log(i);
+// }
+
+// function logging(a, b) {
+//     console.log(a + b)
+// }
+
+// logging(3, 5);
+
+// logging(6, 9);
