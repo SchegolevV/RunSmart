2e3e37af2a2d4f4020986488ab4edeb0b7eab4dd
Valentin <maracas20011@gmail.com>
2022-12-04 19:22:16 +0300

    doing variables of js slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,55 @@
+                                    // by slick-slider
+// $(document).ready(function(){
+//     $('.slider__inner').slick({
+//         speed: 500,
+//         adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="icons/list-four/chevron-left-solid.svg"></img></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/list-four/chevron-right-solid.svg"></img></button>',
+//         responsive: [
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                   dots: true,
+//                   arrows: false
+//                 }
+//             }
+//         ]
+//     });
+// });
+                               // by tiny slider
+// const slider = tns({
+//     container: '.slider__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     controls: false,
+//     nav: false
+//   });
+
+// document.querySelector('.prev').addEventListener('click', function () {
+//     slider.goTo('prev');
+//   });
+
+// document.querySelector('.next').addEventListener('click', function () {
+//     slider.goTo('next');
+//   });
+
 $(document).ready(function(){
-    $('.slider__inner').slick({
-        speed: 500,
-        adaptiveHeight: false,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/list-four/chevron-left-solid.svg"></img></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/list-four/chevron-right-solid.svg"></img></button>',
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                  dots: true,
-                  arrows: false
-                }
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoWidth:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
             }
-        ]
+        }
     });
-});
+  });
